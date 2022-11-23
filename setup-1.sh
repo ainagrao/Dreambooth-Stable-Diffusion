de11a5fc47f09be7cb614e7c770dafa7b2cdabaa
Nagesh Rao <nagesh@antarix.com>
2022-11-23 12:25:00 +0530

    shell scripts for setup

---
+++ setup-1.sh
@@ -0,0 +1,41 @@
+cd /workspace
+# Notes: do everything in workspace
+
+# OLD - git clone https://github.com/JoePenna/Dreambooth-Stable-Diffusion 
+git clone https://github.com/ainagrao/Dreambooth-Stable-Diffusion.git
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh
+
+chmod +x Miniconda3-py38_4.12.0-Linux-x86_64.sh
+./Miniconda3-py38_4.12.0-Linux-x86_64.sh -b -p $HOME/miniconda3
+
+
+# Notes: silent install with -b -p $HOME/miniconda3 did not work. i.e, conda not found after logout and login
+
+# * Logout and login again
+
+cd /workspace
+
+cd Dreambooth-Stable-Diffusion; 
+git checkout devel
+
+# Notes: use branch devel for bleeding edge changes
+
+conda env create -f environment.yaml
+# Notes: This takes time. TODO - how to move this to docker. How much time does it take?
+
+conda activate ldm
+
+python dl_model.py $1
+# Notes: download model.ckpt (from own google drive) using gdown
+
+git clone https://github.com/djbielejeski/Stable-Diffusion-Regularization-Images-person_ddim.git
+
+mkdir -p regularization_images/person_ddim
+# Notes: dataset="person_ddim" #@param ["man_euler", "man_unsplash", "person_ddim", "woman_ddim", "blonde_woman"]
+
+mv -v Stable-Diffusion-Regularization-Images-person_ddim/person_ddim/*.* regularization_images/person_ddim
+
+mkdir training_images
+
+#TODO: move all commands above to a shell script
